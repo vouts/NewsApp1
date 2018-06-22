6c45d2986fd973014e7fa3460bb664e6a9326f60
vouts <thevouts@gmail.com>
2018-06-22 11:01:05 +0300

    v 2.0

--- app/src/main/java/com/example/android/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.newsapp;
 
 import android.content.SharedPreferences;
@@ -31,19 +17,19 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment
+    public static class NewsPreferenceFragment extends PreferenceFragment
             implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-           // addPreferencesFromResource(R.xml.settings_main);
+            addPreferencesFromResource(R.xml.settings_main);
 
-            //Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
-           // bindPreferenceSummaryToValue(minMagnitude);
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
 
-          //  Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
-          //  bindPreferenceSummaryToValue(orderBy);
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         @Override
